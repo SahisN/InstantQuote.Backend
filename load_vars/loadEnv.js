d18acbfd1a83d09e82fcf60e3bcc361562537a3d
SahisN <89941586+SahisN@users.noreply.github.com>
2025-05-21 13:14:09 -0700

    Update loadEnv.js

--- load_vars/loadEnv.js
+++ load_vars/loadEnv.js
@@ -1,12 +1,7 @@
-import dotenv from "dotenv";
-
-// dotenv.config();
 
 export const dbUsername = process.env.DB_USERNAME;
 export const dbPassword = process.env.DB_PASSWORD;
 export const dbAppname = process.env.DB_APPNAME;
 export const sessionSecret = process.env.SESSION_SECRET;
-export const allowedConnection = process.env.ALLOWED_CONNECTION
-  ? process.env.ALLOWED_CONNECTION
-  : "http://localhost:5173";
+export const allowedConnection = process.env.ALLOWED_CONNECTION;
 export const secure = process.env.SECURE ? true : false;
