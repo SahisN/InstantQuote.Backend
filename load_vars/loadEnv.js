047b313d1807d38cf72edbb31ebbf3a364d7bfc3
Sahis <sahisneupane23@gmail.com>
2025-05-22 22:50:25 -0700

    Remove allowedConnection and secondaryConnection from loadEnv.js

--- load_vars/loadEnv.js
+++ load_vars/loadEnv.js
@@ -2,5 +2,3 @@ export const dbUsername = process.env.DB_USERNAME;
 export const dbPassword = process.env.DB_PASSWORD;
 export const dbAppname = process.env.DB_APPNAME;
 export const sessionSecret = process.env.SESSION_SECRET;
-export const allowedConnection = process.env.ALLOWED_CONNECTION;
-export const secondaryConnection = process.env.SECONDARY_CONNECTION;
