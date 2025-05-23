0f614b656c61148ff89f6908469c99d3c7b8e0b8
SahisN <89941586+SahisN@users.noreply.github.com>
2025-05-22 19:24:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(
     secret: sessionSecret,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true, httpOnly: false, sameSite: "none" }, // use true if using HTTPS
+    cookie: { secure: true, httpOnly: true, sameSite: "none" }, // use true if using HTTPS
   })
 );
 
